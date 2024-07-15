4937147988175fc26c5e2968652f170ce02a7314
Banurr <yermagambetovnurbol@gmail.com>
2024-07-15 17:28:06 +0500

    started adding test using junit and mockito

--- src/main/java/com/banurr/pet_project/dto/CompanyDto.java
+++ src/main/java/com/banurr/pet_project/dto/CompanyDto.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
--- src/test/java/com/banurr/pet_project/PetProjectApplicationTests.java
+++ src/test/java/com/banurr/pet_project/PetProjectApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PetProjectApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
+
 	}
 
 }
